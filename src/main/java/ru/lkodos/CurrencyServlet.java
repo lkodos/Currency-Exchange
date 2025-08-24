fdbaf3739644c4daf78da5af68d3d920fee8a856
lkodos <tshagimardanov@gmail.com>
2025-08-24 22:41:17 +0300

    finish getAllCurrencies

---
+++ src/main/java/ru/lkodos/CurrencyServlet.java
@@ -0,0 +1,32 @@
+package ru.lkodos;
+
+import com.google.gson.Gson;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.lkodos.dto.CurrencyDto;
+import ru.lkodos.service.CurrencyService;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@WebServlet("/currencies")
+public class CurrencyServlet extends HttpServlet {
+
+    private final CurrencyService currencyService = CurrencyService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        List<CurrencyDto> allCurrencies = currencyService.getAllCurrencies();
+        String currencies = new Gson().toJson(allCurrencies);
+        try (var writer = resp.getWriter()) {
+            writer.write(currencies);
+            writer.flush();
+        }
+    }
+}
