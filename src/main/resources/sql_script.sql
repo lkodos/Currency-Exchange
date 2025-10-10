d18f159c461049f3ad7e2f3ce45d70c3218a37cb
lkodos <tshagimardanov@gmail.com>
2025-10-10 15:24:03 +0300

    add resources and dependencies

---
+++ src/main/resources/sql_script.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS currency;
+
+CREATE TABLE IF NOT EXISTS currency (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    code VARCHAR(3) UNIQUE NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    sign VARCHAR(10)
+);
+
+INSERT INTO currency(code, name, sign)
+VALUES ('RUB', 'Russian Ruble', '₽'),
+       ('EUR', 'Euro', '€'),
+       ('USD', 'US Dollar', '$');
