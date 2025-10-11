8347b239d12985aa4b0dd5af753e9c23da017567
lkodos <tshagimardanov@gmail.com>
2025-10-11 11:47:39 +0300

    simple connection to db

---
+++ src/main/java/ru/lkodos/util/ConnectionManager.java
@@ -0,0 +1,36 @@
+package ru.lkodos.util;
+
+import ru.lkodos.exception.DbAccessException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static final String URL = "db.url";
+    private static final String DRIVER = "db.driver";
+
+    static {
+        loadDriver();
+    }
+
+    private ConnectionManager() {
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(PropertiesUtil.getProperties(URL));
+        } catch (SQLException e) {
+            throw new DbAccessException("Database is unavaliable", e);
+        }
+    }
+
+    private static void loadDriver() {
+        try {
+            Class.forName(PropertiesUtil.getProperties(DRIVER));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
