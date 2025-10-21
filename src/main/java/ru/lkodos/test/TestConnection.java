1dba4a482ebc7744f7e1d997135a504bfa7c6cbe
lkodos <tshagimardanov@gmail.com>
2025-10-21 11:03:14 +0300

    connection to db

---
+++ src/main/java/ru/lkodos/test/TestConnection.java
@@ -0,0 +1,19 @@
+package ru.lkodos.test;
+
+import ru.lkodos.util.ConnectionManager;
+
+import java.sql.Connection;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        try {
+            Connection connection = ConnectionManager.getDataSource().getConnection();
+            System.out.println("Connection SUCCEEDED");
+        } catch (Exception e) {
+            System.out.println("Connection FAILED");
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/ru/lkodos/util/ConnectionManager.java
@@ -0,0 +1,38 @@
+package ru.lkodos.util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+
+public class ConnectionManager {
+
+    private static HikariDataSource dataSource;
+
+    private static final String URL = "db.url";
+    private static final String DRIVER = "db.driver";
+    private static final String MAX_POOL_SIZE = "max.pool.size";
+    private static final String MIN_IDLE = "min.idle";
+    private static final String CONNECTION_TIMEOUT = "connection.timeout";
+    private static final String IDLE_TIMEOUT = "idle.timeout";
+    private static final String MAX_LIFETIME = "max.lifetime";
+
+    private ConnectionManager() {
+    }
+
+    public static DataSource getDataSource() {
+        if (dataSource == null) {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(PropertiesUtil.getProperties(URL));
+            config.setDriverClassName(PropertiesUtil.getProperties(DRIVER));
+            config.setMaximumPoolSize(Integer.parseInt(PropertiesUtil.getProperties(MAX_POOL_SIZE)));
+            config.setMinimumIdle(Integer.parseInt(PropertiesUtil.getProperties(MIN_IDLE)));
+            config.setConnectionTimeout(Integer.parseInt(PropertiesUtil.getProperties(CONNECTION_TIMEOUT)));
+            config.setIdleTimeout(Integer.parseInt(PropertiesUtil.getProperties(IDLE_TIMEOUT)));
+            config.setMaxLifetime(Integer.parseInt(PropertiesUtil.getProperties(MAX_LIFETIME)));
+
+            dataSource = new HikariDataSource(config);
+        }
+        return dataSource;
+    }
+}
