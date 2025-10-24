acee58b6be45d162f62555b1e0365790eaebb240
lkodos <tshagimardanov@gmail.com>
2025-10-24 10:14:14 +0300

    add get full exchange rates Select

--- src/main/resources/sql_script.sql
+++ src/main/resources/sql_script.sql
@@ -31,15 +31,15 @@ VALUES (1, 2, 2),
        (2, 3, 1.4);
 
 SELECT e.id,
-       e.base_currency_id,
-       c.code,
-       c.name,
-       c.sign,
-       e.target_currency_id,
-       c2.code,
-       c2.name,
-       c2.sign,
-       e.rate
+       e.base_currency_id AS base_id,
+       c.code AS base_code,
+       c.name AS base_name,
+       c.sign AS base_sign,
+       e.target_currency_id AS target_id,
+       c2.code AS target_code,
+       c2.name AS target_name,
+       c2.sign AS target_sign,
+       e.rate AS rate
 FROM exchange_rates e
 JOIN currency c ON c.id = e.base_currency_id
 JOIN currency c2 ON c2.id = e.target_currency_id;
