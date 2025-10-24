b83656ee2667b31f99f5b7e9b9035f4292068449
lkodos <tshagimardanov@gmail.com>
2025-10-24 09:52:56 +0300

    add exchange_rates table

--- src/main/resources/sql_script.sql
+++ src/main/resources/sql_script.sql
@@ -1,13 +1,31 @@
-DROP TABLE IF EXISTS currency;
+PRAGMA foreign_keys = on;
 
-CREATE TABLE IF NOT EXISTS currency (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    code VARCHAR(3) UNIQUE NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    sign VARCHAR(10)
+DROP TABLE IF EXISTS currency;
+DROP TABLE IF EXISTS exchange_rates;
+
+CREATE TABLE IF NOT EXISTS currency
+(
+    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    code TEXT UNIQUE NOT NULL,
+    name TEXT        NOT NULL,
+    sign TEXT
+);
+
+CREATE TABLE IF NOT EXISTS exchange_rates
+(
+    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    base_currency_id INTEGER NOT NULL REFERENCES currency (id),
+    target_currency_id INTEGER NOT NULL REFERENCES currency (id),
+    rate NUMERIC,
+    UNIQUE (base_currency_id, target_currency_id)
 );
 
 INSERT INTO currency(code, name, sign)
 VALUES ('RUB', 'Russian Ruble', '₽'),
        ('EUR', 'Euro', '€'),
        ('USD', 'US Dollar', '$');
+
+INSERT INTO exchange_rates(base_currency_id, target_currency_id, rate)
+VALUES (1, 2, 2),
+       (1, 3, 0.5),
+       (2, 3, 1.4);
